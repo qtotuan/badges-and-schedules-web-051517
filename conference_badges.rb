d8a61e0086783411ac9b5befe12a3ab1e89f705e
Quynh To Tuan <quynh.totuan@gmail.com>
2017-04-26 00:25:35 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+def badge_maker(string)
+  "Hello, my name is #{string}."
+end
+
+def batch_badge_creator(arr)
+  badges = []
+  arr.each do |e|
+    badges << "Hello, my name is #{e}."
+  end
+  badges
+end
+
+def assign_rooms(arr)
+  assignments = []
+  arr.each_with_index do |e, i|
+    assignments << "Hello, #{e}! You'll be assigned to room #{i + 1}!"
+  end
+  assignments
+end
+
+def printer(arr)
+  arr.each do |e|
+    puts badge_maker(e)
+  end
+  assignments = assign_rooms(arr)
+  assignments.each do |e|
+    puts e
+  end
+end
